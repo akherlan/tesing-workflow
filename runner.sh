c0a6f319539a21776ef9f8b551f02f31ff17054b
Andi <herlandi.27@gmail.com>
2022-08-24 16:05:17 +0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # with python 3.8
+which python
 python -m venv venv
 source venv/bin/activate
+which python
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 python test.py
